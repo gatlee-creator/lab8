95180e23a41708fdb3c973b23fc70bed2e4c73ab
gatlee-creator <gatlee@highpoint.edu>
2019-10-08 10:04:57 -0400

    added 4 member methods

--- hybridAuto.h
+++ hybridAuto.h
@@ -3,7 +3,12 @@
 class hybridAutoType : public automobileType {
     public:
         static const bool GAS = false;
-        static const bool ELECTRIC = true; 
+        static const bool ELECTRIC = true;
+
+        float setChargeLevel(float);
+        void getChargeLevel();
+        float setChargeEfficiency(float);
+        void getChargeEfficieny();   
 
     private:
         float bauCharge;
