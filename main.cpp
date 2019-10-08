64b14b516ff35f95cd8879db0ab00313f2f554b0
gatlee-creator <gatlee@highpoint.edu>
2019-10-08 11:12:04 -0400

    sucessfully tested param constructor

--- main.cpp
+++ main.cpp
@@ -10,5 +10,8 @@ int main()
     hybridAutoType hybrid1; 
     cout << hybrid1.getInfoString() << endl; 
 
+    hybridAutoType hybrid2(20, 14.8, 3200, 40, 30.2);
+    cout << hybrid2.getInfoString() << endl;
+
     return 0;
 }
