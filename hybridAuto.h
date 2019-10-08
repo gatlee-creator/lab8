1ae962929dce0a0129af65346d0243d1d88f12eb
gatlee-creator <gatlee@highpoint.edu>
2019-10-08 10:02:11 -0400

    added const vars to hold engineMode value

--- hybridAuto.h
+++ hybridAuto.h
@@ -1,9 +1,13 @@
 #include "automobile.h"
 
 class hybridAutoType : public automobileType {
+    public:
+        static const bool GAS = false;
+        static const bool ELECTRIC = true; 
 
     private:
         float bauCharge;
         float bauEfficieny; 
+        bool engineMode; 
 
 }; 
