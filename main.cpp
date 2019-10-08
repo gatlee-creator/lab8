67aa8c4bbd31051c56e35dd0ca34d4ef3584709a
gatlee-creator <gatlee@highpoint.edu>
2019-10-08 10:29:38 -0400

    succesfully tested hybridAuto constructor in main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "hybridAuto.h"
+
+using namespace std; 
+
+int main()
+{
+
+    hybridAutoType hybrid1; 
+    cout << hybrid1.getInfoString() << endl; 
+
+    return 0;
+}
